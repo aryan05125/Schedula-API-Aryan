bb67675fbd71567b84aaf698accece6ae55645d1
aryan05125 <aryanchauhan144512@gmail.com>
2025-09-14 00:33:17 +0530

    commit-message

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,29 +1,7 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { HelloController } from './hello/hello.controller';
-
-import { User } from './entities/user.entity';
-import { Doctor } from './entities/doctor.entity';
-import { Patient } from './entities/patient.entity';
+import { HelloModule } from './hello/hello.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',   
-      password: 'aryan512', 
-      database: 'internship_db', 
-      entities: [User, Doctor, Patient],
-      synchronize: true,, 
-    }),
-    TypeOrmModule.forFeature([User, Doctor, Patient]),
-  ],
-  controllers: [AppController, HelloController],
-  providers: [AppService],
+  imports: [HelloModule],
 })
 export class AppModule {}
