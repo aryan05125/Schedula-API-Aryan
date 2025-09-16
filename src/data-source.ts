7277f4ec70b921b33bfb3052fa0b2fb5d7402a06
aryan05125 <aryanchauhan144512@gmail.com>
2025-09-16 15:50:38 +0530

    added task3 DatabaseSetup&EntityCreation-ADD env

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import { DataSource } from 'typeorm';
 import { User } from './entities/user.entity';
 import { Doctor } from './entities/doctor.entity';
@@ -5,11 +6,11 @@ import { Patient } from './entities/patient.entity';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'aryan512',
-  database: 'internship_db',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   entities: [User, Doctor, Patient],
   migrations: ['src/migrations/*.ts'],
   synchronize: false, // keep migrations
