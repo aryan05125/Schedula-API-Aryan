022976a329205b434708808b79dee976dcd872c0
aryan05125 <aryanchauhan144512@gmail.com>
2025-09-19 18:14:33 +0530

    fix: use DATABASE_URL for Render

--- src/data-source.ts
+++ src/data-source.ts
@@ -6,11 +6,7 @@ import { Patient } from './entities/patient.entity';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  url: process.env.DATABASE_URL,   // 👈 single connection string
   entities: [User, Doctor, Patient],
   migrations: ['src/migrations/*.ts'],
   synchronize: false, // keep migrations
